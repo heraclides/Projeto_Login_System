dfdc4e92646a5e5cc1cebe826579d5f64339d125
Heraclides <hcm1978@gmail.com>
2026-07-17 19:41:26 -0300

    criando backend e testando rotas

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+ //const PORT = 3000;
+
+app.use(express.static(path.join(__dirname, '../frontend')));
+
+app.get('/', (req,res)=>{
+
+res.sendFile(path.join(__dirname,'../frontend/index.html'));
+
+});
+
+app.listen(3000,()=>{
+
+console.log("Servidor iniciado");
+
+
+// app.listen(PORT,()=>{
+
+// console.log(`Servidor rodando em http://localhost:${PORT}`);
+
+});
